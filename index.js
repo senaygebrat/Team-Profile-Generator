a32bdcdfa8461915a42cb20d39cdea33da0a058e
Senay Gebrat <senaygebrat@gmail.com>
2022-10-23 20:20:46 -0700

    added functions

--- index.js
+++ index.js
@@ -1,54 +1,152 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const employee = require('./lib/employee')
-const engineer = require('./lib/engineer')
-const intern = require('./lib/intern')
-const manager = require('./lib/manager')
+const Engineer = require('./lib/engineer')
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
 const teamMembers = []
+const htmlTemplate = ''
 console.log('Build your team!')
 
-function init(){
-inquirer.prompt([{
-    type: 'list',
-    name: 'typeEmployee',
-    message: "What type of employee are you?",
-    choices: ['manager', 'intern', 'engineer'],
 
-    //switch statement, then response first
-    // response.memberchoice
-}])
+function generateHtml(){
+  let data = htmlTemplate + `</body>
+  </html>`
+fs.writeFile('./index.html', data, function(err){
+  if(err ? console.log(err) : console.log('Success'));
+})
+console.log(teamMembers)
 }
 
 function createManager(){
-  inquirer.prompt([{
+  inquirer.prompt([
+  {
     type: 'input',
     name: 'name',
-    message: "What is the manager's name?"
-  }])
+    message: "What is the team manager's name?"
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the team manager's id number?"
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the team manager's email?"
+  },
+  {
+    type: 'input',
+    name: 'office',
+    message: "What is the team manager's office number?"
+  },
+  {
+    type: 'checkbox',
+    name: 'typeEmployee',
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+  }
+])
+.then((response) => {
+  if (response.typeEmployee == "Engineer"){
+    createEngineer()
+  }
+  else if(response.typeEmployee == "Intern"){
+    createIntern()
+  }
+  else if(response.typeEmployee == "I don't want to add any more team members"){
+    generateHtml()
+  }
+  const newEmployee = new Manager(response.name, response.id, response.email, response.office)
+  teamMembers.push(newEmployee)
+})
 }
 
+
 function createIntern(){
-  inquirer.prompt([{
+  inquirer.prompt([
+  {
     type: 'input',
     name: 'name',
     message: "What is the intern's name?",
-
+  },
+  {
     type: 'input',
-    name: 'name',
-    message: "What is the intern's name?"
-  }])
+    name: 'id',
+    message: "What is the intern's id number?"
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the intern's email?"
+  },
+  {
+    type: 'input',
+    name: 'school',
+    message: "What is your intern's school?"
+  },
+  {
+    type: 'checkbox',
+    name: 'typeEmployee',
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+  }
+])
+.then((response) => {
+  if (response.typeEmployee == "Engineer"){
+    createEngineer()
+  }
+  else if(response.typeEmployee == "Intern"){
+    createIntern()
+  }
+  else if(response.typeEmployee == "I don't want to add any more team members"){
+    generateHtml()
+  }
+  
+})
 }
+
 
 function createEngineer(){
-  inquirer.prompt([{
+  inquirer.prompt([
+  {
     type: 'input',
     name: 'name',
-    message: "What is the intern's id?"
-  }])
-  .then((response) => {
-    const selectEngineer = new Engineer(response.name, response.id, response.email, response.github)
-  })
+    message: "What is the engineer's name?"
+  },
+  {
+    type: 'input',
+    name: 'id',
+    message: "What is the engineer's id number?"
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: "What is the engineer's email?"
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: "What is the engineer's Github username?"
+  },
+  {
+    type: 'checkbox',
+    name: 'typeEmployee',
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+  }
+])
+.then((response) => {
+  if (response.typeEmployee == "Engineer"){
+    createEngineer()
+  }
+  else if(response.typeEmployee == "Intern"){
+    createIntern()
+  }
+  else if(response.typeEmployee == "I don't want to add any more team members"){
+    generateHtml()
+  }
+})
 }
 
 
-init();
+createManager();
